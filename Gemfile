b3b0a98ae1cb8d91ec361a65239876939c739e9b
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2023-09-16 02:42:34 -0300

    feat: :art: Validator cpf, email

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 gem 'rails-i18n'
+gem "cpf_cnpj"
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,3 +1,23 @@
 class Municipe < ApplicationRecord
+  require "cpf_cnpj"
   validates :nome, :cpf, :cns, :email, :data_nascimento, :telefone, :foto, :status, :presence => true
+
+  validate :verify_cpf
+  validate :verify_cns
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+
+  def verify_cpf
+    if cpf.present?
+      errors.add(:base, 'Cpf informado é inválido. Favor verificar e informar um cpf válido') if !CPF.valid?(cpf)
+    end  
+  end
+  
+  def verify_cns
+    if cpf.present?
+      errors.add(:base, 'Cns informado é inválido. Favor verificar e informar um cns válido') if cns.present?
+    end  
+  end
+
 end
